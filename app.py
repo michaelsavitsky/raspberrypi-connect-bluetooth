3805bf65d57426044d3805c909790f2c1982de06
Michael Savitsky <michael.savitsky@gmail.com>
2022-03-13 21:58:43 -0400

    Basic socket and a custom audio file for later testing

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 import bluetooth
+import subprocess
 
-print("Searching for bluetooth devices")
-
-device_name = "SRS-XB21"
 device_addr = None
+device_name = "SRS-XB21"
+passkey = "0000"
+port = 1
+print("Searching for", device_name)
 
-devices = bluetooth.discover_devices()
+devices = bluetooth.discover_devices(duration=3, lookup_names=True)
 
-for addr in devices:
-	if device_name == bluetooth.lookup_name(addr):
+for addr,name in devices:
+	if device_name == name:
 		device_addr = addr
 		break
 
-print(devices)
-print(device_name)
-print(device_addr)
+if device_addr is not None:
+	print("Device not found. Address:", device_addr)
+else:
+	print("Device not found")
+
+# I found this part on StackOverflow, because of course I did
+subprocess.call("kill -9 `pidof bluetooth-agent`", shell=True)
+
+status = subprocess.call("bluetooth_agent " + passkey + " &", shell=True)
+
+try:
+	socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+	socket.connect((device_addr, port))
+except bluetooth.BluetoothError as err:
+	print("Something went wrong idk")
+	socket.close()
+	pass
+
+socket.recv(1024)
+socket.send("What do I even put here")
+
+socket.close()
